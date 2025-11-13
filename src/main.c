ccb58be2a3f6a3ebc841db68f837e311dc7ec9f4
deanqx <110404616+deanqx@users.noreply.github.com>
2025-11-13 14:26:27 +0100

    feat: trying to upload to AT90CAN32

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+static const uint8_t TXD1 = PD3;
+
 int main(void) {
-  DDRB = 1 << PB5;
+  DDRD = 1 << TXD1;
   while (1) {
-    PORTB ^= 1 << PB5;
+    PORTD ^= 1 << TXD1;
     _delay_ms(500);
   }
   return 0;
