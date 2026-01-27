c29106ca0250d3fb327ec1d72a48ab9cc76481aa
deanqx <110404616+deanqx@users.noreply.github.com>
2026-01-27 17:42:25 +0100

    WIP: trying to use USART on AT90CAN

--- src/cockpit/controller.c
+++ src/cockpit/controller.c
@@ -1,19 +1,16 @@
-#include "hal.h"
 #include "usart.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
 void main_loop(void) {
-  hal_init();
-
-  uart0_puts("JCS-BK_1091_GO-KART\r\n");
-  uart0_puts("Lenkrad und Anzeige\r\n");
-  uart0_puts("von Dean Schneider (GYT26)\r\n");
+  // uart0_puts("JCS-BK_1091_GO-KART\r\n");
+  // uart0_puts("Lenkrad und Anzeige\r\n");
+  // uart0_puts("von Dean Schneider (GYT26)\r\n");
+  // DDRD = 1 << PD3;
+  uart1_init(BAUD_CALC(9600UL));
 
   while (1) {
-    hal_io_toggle(PORT_TXD1, TXD1);
-
-    hal_apply();
+    uart1_puts("bL.pic=1\xFF\xFF\xFF");
     _delay_ms(500);
   }
 }
